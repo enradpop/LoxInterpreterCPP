bf0e0764761ac8c050b10f1923bea6b3922bd310
enradpop <bursuq@gmail.com>
2024-01-23 18:31:50 +0200

    Clarify ownership in AST
    Expresion members are now unique pointers.
    Terminal members(tokens) are now plain objects.
    AST visitor takes ownership of Expr when visiting nodes.

--- AstPrinter/AstPrinter.cpp
+++ AstPrinter/AstPrinter.cpp
@@ -3,47 +3,49 @@
 #include <sstream>
 #include <iostream>
 #include "../Expr.h"
+#include <cassert> 
 //TODO Should be templated on return type?
 class AstPrinter : public Visitor<std::string> {
 public:
-    std::string parenthesize(std::string name, std::vector<Expr<std::string>*> exprs) {
+    std::string parenthesize(std::string name, std::vector<std::unique_ptr<Expr<std::string>>>& exprs) const {
         std::ostringstream builder;
         builder << "(" << name;
-        for(auto e : exprs) {
-            builder << " " << e->accept(this);
+        for(auto& e : exprs) {
+            builder << " " << e->accept(*this);
         }
         builder << ")";
         return builder.str();
     }
 
-    std::string print(Expr<std::string>* expr) {
-        return expr->accept(this);
+    std::string print(Expr<std::string>* expr) const {
+        return expr->accept(*this);
     }
 
-    std::string visitBinaryExpr(Binary<std::string>* expr) override {
-        std::vector<Expr<std::string>*> vect;
-        vect.push_back(expr->left);vect.push_back(expr->right);
-        return parenthesize(expr->oprtr->_lexeme, vect);
+    std::string visitBinaryExpr(Binary<std::string>& expr) const override {
+        std::vector<std::unique_ptr<Expr<std::string>>> vect;
+        vect.push_back(std::move(expr.left));vect.push_back(std::move(expr.right));
+        return parenthesize(expr.oprtr._lexeme, vect);
     }
 
-    std::string visitGroupingExpr(Grouping<std::string>* expr) override {
-        std::vector<Expr<std::string>*> vect;
-        vect.push_back(expr->expression);
+    std::string visitGroupingExpr(Grouping<std::string>& expr) const override {
+        std::vector<std::unique_ptr<Expr<std::string>>> vect;
+        vect.push_back(std::move(expr.expression));
         return parenthesize("group", vect);
     }
 
-    std::string visitLiteralExpr(Literal<std::string>* expr) override { 
-        if(expr->value->_type == Token::STRING)
-            return std::get<std::string>(*(expr->value->_literal));
-        else if (expr->value->_type == Token::NUMBER)
-            return  std::to_string(std::get<double>(*(expr->value->_literal)));
-        else return "nil";//TODO exc?
+    std::string visitLiteralExpr(Literal<std::string>& expr) const override { 
+        if(expr.value._type == Token::STRING)
+            return std::get<std::string>(*(expr.value._literal));
+        else if (expr.value._type == Token::NUMBER)
+            return  std::to_string(std::get<double>(*(expr.value._literal)));
+        assert(true);
+        return "nil";
     }
 
-    std::string visitUnaryExpr(Unary<std::string>* expr) override {
-        std::vector<Expr<std::string>*> vect;
-        vect.push_back(expr->right);
-        return parenthesize(expr->oprtr->_lexeme, vect);
+    std::string visitUnaryExpr(Unary<std::string>& expr) const override {
+        std::vector<std::unique_ptr<Expr<std::string>>> vect;
+        vect.push_back(std::move(expr.right));
+        return parenthesize(expr.oprtr._lexeme, vect);
     }
 };
 
@@ -51,12 +53,13 @@ int main() {
     //(* (- 123) (group 45.67))
     Expr<std::string>* expression = new Binary<std::string>(
         new Unary<std::string>(
-            new Token(Token::MINUS, "-", std::nullopt, 1),
-            new Literal<std::string>(new Token(Token::NUMBER, "123", 123.0, 1))),
-        new Token(Token::STAR, "*", std::nullopt, 1),
+            {Token::MINUS, "-", std::nullopt, 1},
+            new Literal<std::string>({Token::NUMBER, "123", 123.0, 1})),
+        {Token::STAR, "*", std::nullopt, 1},
         new Grouping<std::string>(
-            new Literal<std::string>(new Token(Token::NUMBER, "45", 45.0, 1))));
+            new Literal<std::string>({Token::NUMBER, "45", 45.0, 1})));
 
-    AstPrinter* printer = new AstPrinter;
-    std::cout << printer->print(expression);
+    AstPrinter printer;
+    std::cout << printer.print(expression);
+    delete expression;
 }
--- tool/GenerateAst.py
+++ tool/GenerateAst.py
@@ -1,10 +1,16 @@
 import sys
 import os
+from enum import Enum
+
+class TokenType(Enum):
+    TOKEN = 1
+    EXPR = 2
 
 def defineAst(outputDir, baseName, types):
     path = os.path.join(outputDir, f"{baseName[:-3]}.h")
     derivedClasses = []
     with open(path, "w") as writer:
+        writer.write("#include <memory>\n")
         writer.write("#include \"Token.h\"\n")
         writer.write("// forward declarations\n")
         writer.write(f"template <typename R> class {baseName[:-3]};\n")
@@ -14,7 +20,8 @@ def defineAst(outputDir, baseName, types):
         writer.write("template <typename R> class Visitor;\n");
         writer.write("\n")
         writer.write(f"template <typename R> class {baseName[:-3]} {{\n")
-        str = "public:\n\tvirtual R accept(Visitor<R>* visitor) = 0;\n"
+        str = "public:\n\tvirtual R accept(Visitor<R> const& visitor) = 0;\n"
+        str += "\tvirtual ~Expr(){};\n"
         writer.write(str.expandtabs(4))
         for type in types:
             className = type.split(":")[0].strip()
@@ -31,20 +38,43 @@ def defineType(baseName, className, fieldList):
     writer = f"template <typename R> class {className}: public {baseName} {{\npublic:\n"
     #constructor
     fields = fieldList.split(", ")
-    writer += f"\t{className}({fieldList}): " 
+    writer += f"\t{className}(" 
     for field in fields:
-        name = field.split(" ")[1]
+        type, name = getTypeAndName(field)
+        if whatType(type) == TokenType.TOKEN:
+            type = f"{type} const&"
+        else:
+            type = f"{type}*"
+        writer += f"{type} {name}, "
+    writer = writer[:-2]
+    writer += f"): "
+    for field in fields:
+        type, name = getTypeAndName(field)
         writer += f"{name}({name}), "
     writer = writer[:-2]
     writer += f"\n\t{{}}\n\n"
+    #destructor
+    writer += f"\t~{className}() override {{}}\n\n"
     #fields
-    writer += "\tR accept(Visitor<R>* visitor) override {\n"
-    writer += f"\t\treturn visitor->visit{className}{baseName[:-3]}(this);\n"
+    writer += "\tR accept(Visitor<R> const& visitor) override {\n"
+    writer += f"\t\treturn visitor.visit{className}{baseName[:-3]}(*this);\n"
     writer += "\t}\n\n"
     for field in fields:
-        writer += f"\t{field};\n"
+        type, name = getTypeAndName(field)
+        if whatType(type) == TokenType.EXPR:
+            type = f"std::unique_ptr<{type}>"
+        writer += f"\t{type} {name};\n"
     writer += f"}};\n"
     return writer.expandtabs(4)
+
+def getTypeAndName(field):
+    type = field.split(" ")[0]
+    name = field.split(" ")[1]
+    return type, name
+def whatType(type):
+    if(type == "Token"):
+        return TokenType.TOKEN
+    return TokenType.EXPR
 
 def defineVisitor(baseName, types, writer):
     #writer.write("template <typename R> class Visitor {\n")
@@ -52,7 +82,7 @@ def defineVisitor(baseName, types, writer):
     str += "public:\n"
     for type in types:
         typeName = type.split(":")[0].strip()
-        str += f"\tvirtual R visit{typeName}{baseName[:-3]}({typeName}<R>* {baseName[:-3].lower()}) = 0;\n"
+        str += f"\tvirtual R visit{typeName}{baseName[:-3]}({typeName}<R>& {baseName[:-3].lower()}) const = 0;\n"
 
     str += "};"
     writer.write(str.expandtabs(4))
@@ -63,7 +93,7 @@ if(len(sys.argv) != 2):
     print("Usage: generate_ast <output directory>")
     exit(64)
 outputDir = sys.argv[1]
-defineAst(outputDir, "Expr<R>", ["Binary   : Expr<R>* left, Token* oprtr, Expr<R>* right",
-                              "Grouping : Expr<R>* expression",
-                              "Literal  : Token* value",
-                              "Unary    : Token* oprtr, Expr<R>* right"])
+defineAst(outputDir, "Expr<R>", ["Binary   : Expr<R> left, Token oprtr, Expr<R> right",
+                              "Grouping : Expr<R> expression",
+                              "Literal  : Token value",
+                              "Unary    : Token oprtr, Expr<R> right"])
